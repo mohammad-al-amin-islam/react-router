b8082ffb0dcf84a6ff2d50afdb0ba0930c91eb94
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-03-29 17:40:04 +0600

    dynamicly details route added

--- src/components/Friend/Friend.js
+++ src/components/Friend/Friend.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Friend = ({ friend }) => {
     const { name, username, id } = friend;
     const navigate = useNavigate();
-    const showFriendDetails = () => {
+    /* const showFriendDetails = () => {
         const path = `/friends/${id}`;
         navigate(path);
-    }
+    } */
     return (
         <div>
             <h2>{name}</h2>
-            <button onClick={showFriendDetails}>{username} id :{id}</button>
-        </div>
+            <Link to={'/friends/' + id}>Show Detail of : {id}</Link>
+            {/* <button onClick={showFriendDetails}>{username} id :{id}</button> */}
+        </div >
     );
 };
 
---
+++ src/components/FriendDetail/FriendDetail.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+const FriendDetail = () => {
+    // const params = useParams(); or
+    const { friendId } = useParams();
+    const [details, setDetails] = useState({});
+    // const { name, email, website, address } = details
+    useEffect(() => {
+        const url = `https://jsonplaceholder.typicode.com/users/${friendId}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setDetails(data));
+    }, []);
+    console.log(details)
+
+    return (
+        <div>
+            <h1>This is the detail of my friend:{friendId}</h1>
+            <h2>Name:{details.name}</h2>
+            <h3>Email:{details.email}</h3>
+            <p>Website:{details.website}</p>
+            <p><small>City:{details.address?.city}</small></p>
+            <p><small>lat:{details.address?.geo?.lat}</small></p>
+
+        </div>
+    );
+};
+
+export default FriendDetail;
