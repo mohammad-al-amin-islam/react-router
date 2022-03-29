820c1b6e5f54e0db54b5a81789903f8b3f58ffab
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-03-29 09:46:14 +0600

    router adder

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>This is about</h1>
+            <p><small>about area is here</small></p>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/components/Friends/Friends.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Friends = () => {
+    return (
+        <div>
+            <h1>I have many friends</h1>
+            <p>thats means i have no friend</p>
+        </div>
+    );
+};
+
+export default Friends;
